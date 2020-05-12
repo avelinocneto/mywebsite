3b8e488ea581d61a2bf992a26a4ae2be42a574ae
Avelino Neto <avelinocarmanhanneto@gmail.com>
2020-05-12 15:27:41 -0300

    Fixing download CV button

--- src/components/printbutton/index.js
+++ src/components/printbutton/index.js
@@ -3,31 +3,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowCircleDown } from '@fortawesome/free-solid-svg-icons';
 import { Translate, withLocalize } from "react-localize-redux";
 
-const pxToMm = (px) => {
-	return Math.floor(px / document.getElementById('myMm').offsetHeight);
-};
-
-const mmToPx = (mm) => {
-	return document.getElementById('myMm').offsetHeight * mm;
-};
-
-const range = (start, end) => {
-	return Array(end - start).join(0).split(0).map(function (val, id) { return id + start });
-};
-
 const printbutton = ({ id, label }) => (
 	<>
 		<div id="myMm" style={{ height: "1mm" }} ></div>
-		<button className="but brd ico white mt-30 ml-10 no-print"
-			onClick={() => {
+		<a target="_blank" rel="noopener noreferrer" href={process.env.PUBLIC_URL + "/assets/pdf/AvelinoCNeto_CurriculumVitae.pdf"}>
+			<button className="but brd ico white mt-30 ml-10 no-print"
+				onClick={() => {
 
 
 
-			}}
-		>
-			{label}
-			<FontAwesomeIcon icon={faArrowCircleDown}></FontAwesomeIcon>
-			<Translate id='label.downloadCV' />
-		</button>
+				}}
+			>
+				{label}
+				<FontAwesomeIcon icon={faArrowCircleDown}></FontAwesomeIcon>
+				<Translate id='label.downloadCV' />
+			</button>
+		</a>
 	</>);
 export default withLocalize(printbutton);
